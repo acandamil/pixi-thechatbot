aa091451183f8b56703ac742ae10be0e6f49d324
Aurora Candamil <arorua96@gmail.com>
2025-01-28 16:59:57 +0000

    first version working

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,25 @@
+import assert from "assert";
+import { type NextRequest } from "next/server";
+
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
+const apiKey = process.env.X_API_KEY;
+assert(apiKey !== undefined);
+
+export async function POST(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("query");
+  const email = searchParams.get("email");
+  const response = await fetch(
+    "https://testfe.voltquant.uk/test/chat?query=" + query + "&email=" + email,
+    {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "x-api-key": apiKey,
+      },
+    }
+  );
+  const text = await response.json();
+  const data = text.data;
+  return Response.json({ answer: data.answer });
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,76 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+import { Send, Zap } from "lucide-react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [message, setMessage] = useState("");
+  const [conversation, setConversation] = useState<
+    { type: string; text: string }[]
+  >([]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const send = async () => {
+    const temporalMessage = message;
+    const newConversation = [
+      ...conversation,
+      { type: "user", text: temporalMessage },
+    ];
+    setConversation(newConversation);
+    setMessage("");
+    const response = await fetch(
+      "/api/chat?query=" + temporalMessage + "&email=auroracandamil",
+      {
+        method: "POST",
+        mode: "cors",
+      }
+    );
+    const json = await response.json();
+    const updatedConversation = [
+      ...newConversation,
+      { type: "bot", text: json.answer },
+    ];
+    setConversation(updatedConversation);
+  };
+  return (
+    <div className="flex flex-col w-screen h-screen bg-slate-100">
+      <div className="flex-none p-4 bg-slate-100 font-bold shadow ">
+        <div className="flex justify-center align-middle">
+          <img
+            src="https://framerusercontent.com/images/QkIRNQNrgeQBQ84TUFiTmH2vpo.png"
+            alt="Voltquant Logo"
+            className="h-20"
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+      </div>
+      <div className="flex-1 p-4 overflow-auto">
+        {conversation.map((entry, index) => (
+          <div
+            key={index}
+            className={`mb-4 p-4 rounded-t-lg shadow relative max-w-md w-fit ${
+              entry.type === "user"
+                ? "bg-slate-200 ml-auto rounded-bl-lg text-right"
+                : "bg-slate-50 mr-auto rounded-br-lg"
+            }`}
+          >
+            <p className="break-words">{entry.text}</p>
+          </div>
+        ))}
+      </div>
+      <div className="flex-none mb-6 p-6 bg-slate-100 text-white">
+        <div className="flex gap-2">
+          <input
+            placeholder="Write a question"
+            onChange={(event) => setMessage(event.target.value)}
+            value={message}
+            className="flex-1 p-2 rounded bg-slate-300 text-black"
+          ></input>
+          <Send
+            className="p-2 rounded bg-slate-500 hover:bg-slate-800 transition text-white"
+            size={40}
+            onClick={() => send()}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </div>
+      </div>
     </div>
   );
 }
