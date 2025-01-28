1455d1acc660fdd8b5efd1aa47f807d399b2048e
Aurora Candamil <arorua96@gmail.com>
2025-01-28 17:11:46 +0000

    working with loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 "use client";
 
 import { useState } from "react";
-import { Send, Zap } from "lucide-react";
+import { LoaderCircle, Send } from "lucide-react";
 
 export default function Home() {
   const [message, setMessage] = useState("");
   const [conversation, setConversation] = useState<
     { type: string; text: string }[]
   >([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const send = async () => {
+    setIsLoading(true);
     const temporalMessage = message;
     const newConversation = [
       ...conversation,
@@ -29,6 +31,7 @@ export default function Home() {
       ...newConversation,
       { type: "bot", text: json.answer },
     ];
+    setIsLoading(false);
     setConversation(updatedConversation);
   };
   return (
@@ -55,6 +58,12 @@ export default function Home() {
             <p className="break-words">{entry.text}</p>
           </div>
         ))}
+        {isLoading && (
+          <div className="bg-slate-50 mr-auto rounded-br-lg flex gap-2 text-gray-500 w-fit shadow mb-4 p-4 rounded-t-lg">
+            <LoaderCircle className="animate-spin" size={24} />
+            <span>Loading response...</span>
+          </div>
+        )}
       </div>
       <div className="flex-none mb-6 p-6 bg-slate-100 text-white">
         <div className="flex gap-2">
@@ -62,7 +71,7 @@ export default function Home() {
             placeholder="Write a question"
             onChange={(event) => setMessage(event.target.value)}
             value={message}
-            className="flex-1 p-2 rounded bg-slate-300 text-black"
+            className="flex-1 p-2 px-4 rounded bg-white text-black"
           ></input>
           <Send
             className="p-2 rounded bg-slate-500 hover:bg-slate-800 transition text-white"
